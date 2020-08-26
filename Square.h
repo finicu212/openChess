64d9d28dbe6d3ca6dbe6fd5211b836987bd3c1b3
finicu212 <smurfdeplastic@gmail.com>
2020-08-26 14:54:09 +0300

    square constructors

--- Square.h
+++ Square.h
@@ -3,7 +3,11 @@
 
 class Square
 {
-
 public:
+	Square(bool s, Piece* p)	: shaded(s), pieceHere(p) {};
+	Square(bool s)				: shaded(s), pieceHere(nullptr) {};
+
+private:
+	bool shaded;
 	Piece* pieceHere;
 };
