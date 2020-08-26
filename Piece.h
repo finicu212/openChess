1370269f6fdda89d7f3f921461a25b07950d4026
finicu212 <smurfdeplastic@gmail.com>
2020-08-26 18:13:27 +0300

    class for all chess pieces

--- Piece.h
+++ Piece.h
@@ -20,3 +20,33 @@ class Pawn : public Piece
 public:
 	Pawn(bool isWhite) : Piece(isWhite ? 'P' : 'p') {};
 };
+
+class Rook : public Piece
+{
+public:
+	Rook(bool isWhite) : Piece(isWhite ? 'R' : 'r') {};
+};
+
+class Knight : public Piece
+{
+public:
+	Knight(bool isWhite) : Piece(isWhite ? 'N' : 'n') {};
+};
+
+class Bishop : public Piece
+{
+public:
+	Bishop(bool isWhite) : Piece(isWhite ? 'B' : 'b') {};
+};
+
+class Queen : public Piece
+{
+public:
+	Queen(bool isWhite) : Piece(isWhite ? 'Q' : 'q') {};
+};
+
+class King : public Piece
+{
+public:
+	King(bool isWhite) : Piece(isWhite ? 'K' : 'k') {};
+};
