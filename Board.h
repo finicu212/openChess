7485e0a6ff784c700e8ae47eef78043666154472
finicu212 <smurfdeplastic@gmail.com>
2020-08-26 15:14:09 +0300

    placeholder constructor

--- Board.h
+++ Board.h
@@ -1,9 +1,65 @@
 #pragma once
+#include <iostream>
 #include "Square.h"
+
 
 class Board
 {
-
 public:
-	Square square[8][8];
+	/// <summary> </summary>
+	/// <param name="playingAsWhite"> - set this to true if playing from white's perspective </param>
+	/// <returns> </returns>
+	Board(bool playingAsWhite);
+
+	friend std::ostream& operator<<(std::ostream& os, const Board& b);
+
+private:
+	Square board[8][8];
 };
+
+
+Board::Board(bool playingAsWhite)
+{
+	// opposing player major pieces TODO
+	bool shadeSquare = true;
+	for (int i = 0; i < 8; i++)
+	{
+		board[0][i] = Square(shadeSquare);
+		shadeSquare = !shadeSquare;
+	}
+
+	// opposing player pawns
+	shadeSquare = !shadeSquare;
+	for (int i = 0; i < 8; i++)
+	{
+		board[1][i] = Square(shadeSquare, new Pawn());
+		shadeSquare = !shadeSquare;
+	}
+
+	// empty squares
+	shadeSquare = !shadeSquare;
+	for (int i = 2; i < 6; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			board[i][j] = Square(shadeSquare);
+		}
+		shadeSquare = !shadeSquare;
+	}
+
+	// player pawns
+	shadeSquare = !shadeSquare;
+	for (int i = 0; i < 8; i++)
+	{
+		board[6][i] = Square(shadeSquare, new Pawn());
+		shadeSquare = !shadeSquare;
+	}
+
+	// player major pieces TODO
+	shadeSquare = !shadeSquare;
+	for (int i = 0; i < 8; i++)
+	{
+		board[7][i] = Square(shadeSquare);
+		shadeSquare = !shadeSquare;
+	}
+}
