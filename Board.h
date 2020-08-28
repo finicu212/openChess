288540b0e3cf614740b38883108145b6067cbd5c
finicu212 <smurfdeplastic@gmail.com>
2020-08-28 21:38:57 +0300

    Board.h no longer uses Square obj

--- Board.h
+++ Board.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <iostream>
-#include "Square.h"
-
+#include "Piece.h"
 
 class Board
 {
@@ -13,67 +12,56 @@ public:
 
 	friend std::ostream& operator<<(std::ostream& os, const Board& b);
 
-	Square squareAt(int i, int j) const;
+	Piece* pieceAt(int i, int j) const;
 
 public:
 	bool whiteSide;
-	Square board[8][8];
+	Piece* board[8][8];
 };
 
-Square Board::squareAt(int i, int j) const
+Piece* Board::pieceAt(int i, int j) const
 {
 	return board[i][j];
 }
 
 Board::Board(bool playingAsWhite) : whiteSide(playingAsWhite)
 {
-	bool shadeSquare = !playingAsWhite;
-	board[0][0] = Square(shadeSquare, new Rook(!playingAsWhite));
-	board[0][1] = Square(!shadeSquare, new Knight(!playingAsWhite));
-	board[0][2] = Square(shadeSquare, new Bishop(!playingAsWhite));
-	board[0][5] = Square(!shadeSquare, new Bishop(!playingAsWhite));
-	board[0][6] = Square(shadeSquare, new Knight(!playingAsWhite));
-	board[0][7] = Square(!shadeSquare, new Rook(!playingAsWhite));
-
-	board[0][3 + (1 && !playingAsWhite)] = Square(!shadeSquare, new King(!playingAsWhite));
-	board[0][4 - (1 && !playingAsWhite)] = Square(shadeSquare, new Queen(!playingAsWhite));
-
-	// opposing player pawns
-	//shadeSquare = true;
-	for (int i = 0; i < 8; i++)
+	for (Piece* p : board)
 	{
-		board[1][i] = Square(shadeSquare, new Pawn(!playingAsWhite));
-		shadeSquare = !shadeSquare;
+		p = nullptr;
 	}
 
-	// empty squares
-	shadeSquare = !shadeSquare;
-	for (int i = 2; i < 6; i++)
+	board[0][0] = new Rook(!playingAsWhite);
+	board[0][1] = new Knight(!playingAsWhite);
+	board[0][2] = new Bishop(!playingAsWhite);
+	board[0][5] = new Bishop(!playingAsWhite);
+	board[0][6] = new Knight(!playingAsWhite);
+	board[0][7] = new Rook(!playingAsWhite);
+
+	board[0][3 + (1 && !playingAsWhite)] = new King(!playingAsWhite);
+	board[0][4 - (1 && !playingAsWhite)] = new Queen(!playingAsWhite);
+
+	// opposing player pawns
+	for (int i = 0; i < 8; i++)
 	{
-		for (int j = 0; j < 8; j++)
-		{
-			board[i][j] = Square(shadeSquare);
-			shadeSquare = !shadeSquare;
-		}
-		shadeSquare = !shadeSquare;
+		board[1][i] = new Pawn(!playingAsWhite);
 	}
 
 	// player pawns
 	for (int i = 0; i < 8; i++)
 	{
-		board[6][i] = Square(shadeSquare, new Pawn(playingAsWhite));
-		shadeSquare = !shadeSquare;
+		board[6][i] = new Pawn(playingAsWhite);
 	}
 
-	board[7][0] = Square(!shadeSquare, new Rook(playingAsWhite));
-	board[7][1] = Square(shadeSquare, new Knight(playingAsWhite));
-	board[7][2] = Square(!shadeSquare, new Bishop(playingAsWhite));
-	board[7][5] = Square(shadeSquare, new Bishop(playingAsWhite));
-	board[7][6] = Square(!shadeSquare, new Knight(playingAsWhite));
-	board[7][7] = Square(shadeSquare, new Rook(playingAsWhite));
+	board[7][0] = new Rook(playingAsWhite);
+	board[7][1] = new Knight(playingAsWhite);
+	board[7][2] = new Bishop(playingAsWhite);
+	board[7][5] = new Bishop(playingAsWhite);
+	board[7][6] = new Knight(playingAsWhite);
+	board[7][7] = new Rook(playingAsWhite);
 
-	board[7][4 - (1 && playingAsWhite)] = Square(!shadeSquare, new King(playingAsWhite));
-	board[7][3 + (1 && playingAsWhite)] = Square(shadeSquare, new Queen(playingAsWhite));
+	board[7][4 - (1 && playingAsWhite)] = new King(playingAsWhite);
+	board[7][3 + (1 && playingAsWhite)] = new Queen(playingAsWhite);
 }
 
 std::ostream& operator<<(std::ostream& os, const Board& b)
@@ -85,7 +73,7 @@ std::ostream& operator<<(std::ostream& os, const Board& b)
 		os << (b.whiteSide ? (8 - i) : (i + 1)) << " ";
 		for (int j = 0; j < 8; j++)
 		{
-			os << "| " << b.squareAt(i, j) << " ";
+			os << "| " << b.printPiece(i, j) << " ";
 		}
 		os << "|\n";
 		os << "  +---+---+---+---+---+---+---+---+\n";
