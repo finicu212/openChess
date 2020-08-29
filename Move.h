9af308f1c0356bbfb5656884daccb833d581c12a
finicu212 <smurfdeplastic@gmail.com>
2020-08-29 22:27:14 +0300

    take strings, get data

--- Move.h
+++ Move.h
@@ -12,6 +12,14 @@ public:
 	Move();
 	Move(Pos2D org, Pos2D destination);
 
+	/// <summary>
+	/// Takes two strings and outputs readable data
+	/// </summary>
+	/// <param name="s"> - string that represents the origin square (ex: "e2")</param>
+	/// <param name="d"> - string that represents the destination square (ex: "e4")</param>
+	/// <returns>a Move object</returns>
+	static Move makeMove(std::string s, std::string d);
+
 private:
 	Pos2D src, dest;
 };
@@ -19,3 +27,15 @@ private:
 Move::Move() {};
 
 Move::Move(Pos2D org, Pos2D destination) : src(org), dest(destination) {}
+
+Move Move::makeMove(std::string s, std::string d)
+{
+	Move newMove;
+	newMove.src.x = tolower(s[0]) - 'a';
+	newMove.src.y = s[1] - 49;
+
+	newMove.dest.x = tolower(d[0]) - 'a';
+	newMove.dest.y = d[1] - 49;
+
+	return newMove;
+}
