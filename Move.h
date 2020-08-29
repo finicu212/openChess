0e2537ba7009661a2d0d241d674972616b28d69c
finicu212 <smurfdeplastic@gmail.com>
2020-08-29 22:55:37 +0300

    accessors for src and dest

--- Move.h
+++ Move.h
@@ -20,22 +20,36 @@ public:
 	/// <returns>a Move object</returns>
 	static Move makeMove(std::string s, std::string d);
 
+	Pos2D src();
+
+	Pos2D dest();
+
 private:
-	Pos2D src, dest;
+	Pos2D src_, dest_;
 };
 
 Move::Move() {};
 
-Move::Move(Pos2D org, Pos2D destination) : src(org), dest(destination) {}
+Move::Move(Pos2D org, Pos2D destination) : src_(org), dest_(destination) {}
+
+Pos2D Move::src()
+{ 
+	return src_; 
+}
+
+Pos2D Move::dest()
+{ 
+	return dest_; 
+}
 
 Move Move::makeMove(std::string s, std::string d)
 {
 	Move newMove;
-	newMove.src.x = tolower(s[0]) - 'a';
-	newMove.src.y = s[1] - 49;
+	newMove.src_.x = tolower(s[0]) - 'a';
+	newMove.src_.y = s[1] - 49;
 
-	newMove.dest.x = tolower(d[0]) - 'a';
-	newMove.dest.y = d[1] - 49;
+	newMove.dest_.x = tolower(d[0]) - 'a';
+	newMove.dest_.y = d[1] - 49;
 
 	return newMove;
 }
