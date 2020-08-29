809a15b2c5157f523acc225e89da4c31446a34ed
finicu212 <smurfdeplastic@gmail.com>
2020-08-29 22:42:45 +0300

    overrride for each piece class to allow compile

--- Piece.h
+++ Piece.h
@@ -28,34 +28,64 @@ class Pawn : public Piece
 {
 public:
 	Pawn(bool isWhite) : Piece(isWhite ? 'P' : 'p') {};
+
+	bool isValidMove(const Move& move) override
+	{
+		return false;
+	}
 };
 
 class Rook : public Piece
 {
 public:
 	Rook(bool isWhite) : Piece(isWhite ? 'R' : 'r') {};
+
+	bool isValidMove(const Move& move) override
+	{
+		return false;
+	}
 };
 
 class Knight : public Piece
 {
 public:
 	Knight(bool isWhite) : Piece(isWhite ? 'N' : 'n') {};
+
+	bool isValidMove(const Move& move) override
+	{
+		return false;
+	}
 };
 
 class Bishop : public Piece
 {
 public:
 	Bishop(bool isWhite) : Piece(isWhite ? 'B' : 'b') {};
+
+	bool isValidMove(const Move& move) override
+	{
+		return false;
+	}
 };
 
 class Queen : public Piece
 {
 public:
 	Queen(bool isWhite) : Piece(isWhite ? 'Q' : 'q') {};
+
+	bool isValidMove(const Move& move) override
+	{
+		return false;
+	}
 };
 
 class King : public Piece
 {
 public:
 	King(bool isWhite) : Piece(isWhite ? 'K' : 'k') {};
+
+	bool isValidMove(const Move& move) override
+	{
+		return false;
+	}
 };
