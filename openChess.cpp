c1b9f0cd92d881fa1a676ce2c832221fed33061e
finicu212 <smurfdeplastic@gmail.com>
2020-08-31 13:29:26 +0300

    implement user interaction

--- openChess.cpp
+++ openChess.cpp
@@ -18,15 +18,26 @@ int main()
     std::cout << "Pick your color (w / b): ";
     std::cin >> col;
     g_playingAsWhite = col[0] == 'w' ? true : false;
+
     Board b;
+    bool whitesTurn = true;
+    while (!gameOver(b))
+    {
+        std::cout << b << '\n';
 
-    std::cout << b << '\n';
+        std::string plyMoveSrc, plyMoveDest;
+        std::cout << "Enter move (src_square dest_square):";
+        std::cin >> plyMoveSrc >> plyMoveDest;
 
-    Move firstMove = Move::getMove("e2", "e4");
-    if (g_playingAsWhite)
-        firstMove = firstMove.invert();
+        Move move = Move::getMove(plyMoveSrc, plyMoveDest);
+        if (whitesTurn)
+        {
+            move = move.invert();
+        }
 
-    b.movePiece(firstMove);
+        b.movePiece(move);
+    }
+
 
     std::cout << b << '\n';
 }
