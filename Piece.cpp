27af0b1058cb99393d6992e8f4f8bab3481dca95
finicu212 <smurfdeplastic@gmail.com>
2020-09-02 17:47:41 +0300

    simplified file structure

--- Piece.cpp
+++ Piece.cpp
@@ -1,5 +1,6 @@
-#include "Piece.h"
 #pragma once
+#include "Board.h"
+// including Board.h to reference global variable g_board which is of type Board
 
 char Piece::getArt()
 {
@@ -22,3 +23,36 @@ Bishop::Bishop(bool isWhite) : Piece(isWhite ? 'B' : 'b') {};
 Queen::Queen(bool isWhite) : Piece(isWhite ? 'Q' : 'q') {};
 
 King::King(bool isWhite) : Piece(isWhite ? 'K' : 'k') {};
+
+
+extern Board g_board;
+
+bool Pawn::isValidMove(const Move& move)
+{
+	return false;
+}
+
+bool Rook::isValidMove(const Move& move)
+{
+	return false;
+}
+
+bool Knight::isValidMove(const Move& move)
+{
+	return false;
+}
+
+bool Bishop::isValidMove(const Move& move)
+{
+	return false;
+}
+
+bool Queen::isValidMove(const Move& move)
+{
+	return false;
+}
+
+bool King::isValidMove(const Move& move)
+{
+	return false;
+}
