09c9c224327c0538c2b9a98cf0b50c237794284a
finicu212 <smurfdeplastic@gmail.com>
2020-09-07 12:34:08 +0300

    explain why the game ended

--- openChess.cpp
+++ openChess.cpp
@@ -6,7 +6,7 @@ Board g_board;
 /// <summary> </summary>
 /// <param name="b"> - the chess board object </param>
 /// <returns> why the game ended. 1 = checkmate, 2 = no moves left, 3 = not enough material, TODO add more</returns>
-int gameOver(const Board& b)
+uint8_t gameOver(const Board& b)
 {
 
     return 0;
@@ -19,7 +19,8 @@ int main()
     std::cin >> col;
     g_board.setPlayingAsWhite(col[0] == 'w');
 
-    while (!gameOver(g_board))
+    uint8_t gameStatus;
+    while (gameStatus = gameOver(g_board) == 0)
     {
         std::cout << g_board << '\n';
 
@@ -37,5 +38,20 @@ int main()
         g_board.movePiece(move);
     }
 
-    std::cout << g_board << '\n';
+    bool whiteWon = !g_board.whitesTurn();
+    switch (gameStatus)
+    {
+    case 1:
+        std::cout << "Checkmate! " << (whiteWon ? "white" : "black") << " won!\n";
+        break;
+    case 2:
+        std::cout << "Stalemate! No moves left for " << (!whiteWon ? "white" : "black") << '\n';
+        break;
+    case 3:
+        std::cout << "Stalemate due to lack of material\n";
+        break;
+    default:
+        std::cout << "Game ended because of unknown cause\n";
+        break;
+    }
 }
